e7caf18b546caee5d8d28d87871d02cc7cd19960
Timchinnn <95688018+Timchinnn@users.noreply.github.com>
2025-01-13 20:21:03 +0000

    modified:   src/components/Offer/Offer.jsx 	modified:   src/components/Offer/Offer.module.css

--- src/components/Offer/Offer.jsx
+++ src/components/Offer/Offer.jsx
@@ -64,6 +64,23 @@ const Offer = () => {
             <input type="checkbox" id="coding" name="interest" value="coding" />
           </div>
         </div>
+        <div className={styles.card}>
+          <div className={styles.cardContent}>
+            <p>Станины для Mei Advance  с чипом….. </p>
+            <div className={styles.usedCount}>
+              <p>б/у</p>
+              <p>15</p>
+            </div>
+          </div>
+          <div className={styles.cardAbout}>
+            <div className={styles.pN}>
+              <p>p/n:</p>
+              <p>1234567890111</p>
+              <img src="/Group 9264.png" alt="" />
+            </div>
+            <input type="checkbox" id="coding" name="interest" value="coding" />
+          </div>
+        </div>
       </div>
     </div>
   );
