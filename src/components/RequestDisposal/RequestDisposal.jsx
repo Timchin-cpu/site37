1e4341a2c8c17a522962512e131641e7cd0e4ebb
Timchinnn <95688018+Timchinnn@users.noreply.github.com>
2025-01-16 18:35:57 +0000

    modified:   src/components/RequestDisposal/RequestDisposal.jsx 	modified:   src/components/RequestDisposal/RequestDisposal.module.css

--- src/components/RequestDisposal/RequestDisposal.jsx
+++ src/components/RequestDisposal/RequestDisposal.jsx
@@ -80,10 +80,6 @@ const RequestDisposal = () => {
       [field]: value,
     }));
   };
-  const handleFocus = () => {
-    window.scrollTo(0, 0);
-    document.body.scrollTop = 0;
-  };
   return (
     <div className={styles.container}>
       {" "}
@@ -107,7 +103,6 @@ const RequestDisposal = () => {
             <p>{t("Respectful Name")}</p>
             <input
               type="text"
-              onFocus={handleFocus}
               value={userData.name}
               onChange={(e) => handleInputChange("name", e.target.value)}
             />
