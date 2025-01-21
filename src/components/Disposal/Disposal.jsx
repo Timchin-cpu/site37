31393ff3cf082b43c004395fdab3bb07002681d6
Timchin-cpu <83727119+Timchin-cpu@users.noreply.github.com>
2025-01-21 16:47:31 +0000

    modified:   src/components/Disposal/Disposal.jsx

--- src/components/Disposal/Disposal.jsx
+++ src/components/Disposal/Disposal.jsx
@@ -199,20 +199,6 @@ function Disposal() {
           />
         </div>
       </div>
-      <div className={styles.supportImgs}>
-        <img
-          src="/wp.png"
-          alt=""
-          onClick={() => window.open("https://wa.me/79295022998")}
-        />
-        <img
-          src="/tg.png"
-          alt=""
-          onClick={() => window.open("https://t.me/Bansys_chat")}
-        />
-
-        <img src="mail.png" alt="" onClick={handleMailClick} />
-      </div>
       <div className={styles.mainContent}>
         <div className={styles.main}>
           <div>
@@ -230,6 +216,20 @@ function Disposal() {
                 )}
               </p>
             </div>
+          </div>
+          <div className={styles.supportImgs}>
+            <img
+              src="/wp.png"
+              alt=""
+              onClick={() => window.open("https://wa.me/79295022998")}
+            />
+            <img
+              src="/tg.png"
+              alt=""
+              onClick={() => window.open("https://t.me/Bansys_chat")}
+            />
+
+            <img src="mail.png" alt="" onClick={handleMailClick} />
           </div>
         </div>
       </div>
